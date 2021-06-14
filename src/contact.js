78c1d1edb0013cb088e9e0fe513f92d6924b9020
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-06-14 19:31:01 +0200

    Fix linter errors

--- src/contact.js
+++ src/contact.js
@@ -1,46 +1,43 @@
-const mainDiv = document.createElement('div');
-const contactDiv = document.createElement('div');
-const contactTitle = document.createElement('h2');
-const contactText = document.createElement('p');
-const addressDiv = document.createElement('div');
-const addressTitle = document.createElement('h2');
-const addressText = document.createElement('p');
-const mailDiv = document.createElement('div');
-const mailTitle = document.createElement('h2');
-const mailText = document.createElement('p');
+const mainDiv = document.createElement("div");
+const contactDiv = document.createElement("div");
+const contactTitle = document.createElement("h2");
+const contactText = document.createElement("p");
+const addressDiv = document.createElement("div");
+const addressTitle = document.createElement("h2");
+const addressText = document.createElement("p");
+const mailDiv = document.createElement("div");
+const mailTitle = document.createElement("h2");
+const mailText = document.createElement("p");
 
 const appendElements = (main) => {
-    contactDiv.appendChild(contactTitle);
-    contactDiv.appendChild(contactText);
-    addressDiv.appendChild(addressTitle);
-    addressDiv.appendChild(addressText);
-    mailDiv.appendChild(mailTitle);
-    mailDiv.appendChild(mailText);
-    mainDiv.appendChild(contactDiv);
-    mainDiv.appendChild(addressDiv);
-    mainDiv.appendChild(mailDiv);
-    main.appendChild(mainDiv);
+  contactDiv.appendChild(contactTitle);
+  contactDiv.appendChild(contactText);
+  addressDiv.appendChild(addressTitle);
+  addressDiv.appendChild(addressText);
+  mailDiv.appendChild(mailTitle);
+  mailDiv.appendChild(mailText);
+  mainDiv.appendChild(contactDiv);
+  mainDiv.appendChild(addressDiv);
+  mainDiv.appendChild(mailDiv);
+  main.appendChild(mainDiv);
 };
 
-const setElementValues  = () => {
+const setElementValues = () => {
+  mainDiv.id = "contact-container";
+  contactTitle.textContent = "Phone";
+  addressTitle.textContent = "Address";
 
- mainDiv.id = 'contact-container';
- contactTitle.textContent = 'Phone';
- addressTitle.textContent = 'Address';
+  contactText.textContent = "018-388-2178";
+  addressText.textContent = "278 road hungry ave";
 
- contactText.textContent = '018-388-2178';
- addressText.textContent = '278 road hungry ave';
-
- mailTitle.textContent = 'Mail Address';
- mailText.textContent = 'david@gemal.com';
+  mailTitle.textContent = "Mail Address";
+  mailText.textContent = "david@gemal.com";
 };
 
-const loadFunc = function(main) {
- //addElementIds();
- appendElements(main);
- setElementValues();
- console.warn('Contact page successfully loaded');
+const loadFunc = function (main) {
+  appendElements(main);
+  setElementValues();
+  // console.warn("Contact page successfully loaded");
 };
 
-
-export {loadFunc};
+export { loadFunc };
